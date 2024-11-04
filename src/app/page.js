11c6ca7b7c9a56ec83c5148c1c0b148a2e3047de
Julie <julieglion@gmail.com>
2024-11-04 14:33:44 +0100

    add usability

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+"use client";
+import { useState } from "react";
+
 import Navigation from "@/components/Navigation";
 import { FaApple } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
@@ -12,6 +15,8 @@ import mint from "../images/mint.png";
 import peach from "../images/peach.png";
 
 export default function Home() {
+  const [currentWatchColor, setcurrentWatchColor] = useState(navy);
+
   return (
     <div className="bg-gradient-to-br from-[#C8DCE5] from-0% to-[#B6CCDA] to-100% px-[8rem] py-[2rem] text-white h-[100svh] w-[100svw]">
       <header className="flex justify-between  items-center">
@@ -27,17 +32,17 @@ export default function Home() {
       </header>
 
       <main className="grid auto-cols-auto pt-16">
-        <h1 className="text-[64px] w-[16ch] self-center py-20">
+        <h1 className="text-[64px] leading-tight w-[16ch] self-center py-20">
           <span className="font-bold">The Perfect Moment</span> Between Past And Future.
         </h1>
 
-        <Image src={navy} alt="navy apple watch" className="col-start-2 row-span-2" />
+        <Image src={currentWatchColor} alt="navy apple watch" className="col-start-2 row-span-2 w-[400]" />
         <div className="col-start-3 self-center row-span-2 flex flex-col items-center gap-1">
-          <ChangeCTA bg_color="bg-[#434558]" active="h-[26px] w-[26px] border-[2px] m-[2px] drop-shadow-md" />
+          {currentWatchColor === navy ? <ChangeCTA bg_color="bg-[#434558]" active="h-[26px] w-[26px] border-[2px] m-[2.5px] drop-shadow-md" currentWatchColor={navy} setcurrentWatchColor={setcurrentWatchColor} /> : <ChangeCTA bg_color="bg-[#434558]" currentWatchColor={navy} setcurrentWatchColor={setcurrentWatchColor} />}
           <div className="border-l-[2px] border-dotted h-[18px] border-white"></div>
-          <ChangeCTA bg_color="bg-[#6addcc]" />
+          {currentWatchColor === mint ? <ChangeCTA bg_color="bg-[#6addcc]" active="h-[26px] w-[26px] border-[2px] m-[2.5px] drop-shadow-md" currentWatchColor={mint} setcurrentWatchColor={setcurrentWatchColor} /> : <ChangeCTA bg_color="bg-[#6addcc]" currentWatchColor={mint} setcurrentWatchColor={setcurrentWatchColor} />}
           <div className="border-l-[2px] border-dotted h-[18px] border-white"></div>
-          <ChangeCTA bg_color="bg-[#f9cdc4]" />
+          {currentWatchColor === peach ? <ChangeCTA bg_color="bg-[#f9cdc4]" active="h-[26px] w-[26px] border-[2px] m-[2.5px] drop-shadow-md" currentWatchColor={peach} setcurrentWatchColor={setcurrentWatchColor} /> : <ChangeCTA bg_color="bg-[#f9cdc4]" currentWatchColor={peach} setcurrentWatchColor={setcurrentWatchColor} />}
         </div>
         <button className="text-[24px] px-[60px] py-[10px] rounded-full border-2 border-white place-self-start hover:bg-white hover:text-[#B6CCDA] font-medium">Buy Now</button>
         <div className="flex gap-[16px] items-center self-end pb-8">
@@ -50,9 +55,9 @@ export default function Home() {
           </button>
         </div>
         <div className="flex gap-6 place-self-end">
-          <WatchColorType image_src={navy} image_alt="navy watch" watch_color="bg-[#434558]" />
-          <WatchColorType image_src={mint} image_alt="mint watch" watch_color="bg-[#6addcc]" />
-          <WatchColorType image_src={peach} image_alt="peach watch" watch_color="bg-[#f9cdc4]" />
+          <WatchColorType image_src={navy} setcurrentWatchColor={setcurrentWatchColor} image_alt="navy watch" watch_color="bg-[#434558]" />
+          <WatchColorType image_src={mint} setcurrentWatchColor={setcurrentWatchColor} image_alt="mint watch" watch_color="bg-[#6addcc]" />
+          <WatchColorType image_src={peach} setcurrentWatchColor={setcurrentWatchColor} image_alt="peach watch" watch_color="bg-[#f9cdc4]" />
         </div>
       </main>
     </div>
--- src/components/ChangeCTA.jsx
+++ src/components/ChangeCTA.jsx
@@ -1,5 +1,16 @@
-const ChangeCTA = ({ bg_color, active }) => {
-  return <div className={`h-[24px] w-[24px] rounded-full border-[1px] border-white ${bg_color} ${active} m-[3px] cursor-pointer hover:h-[26px] hover:w-[26px] hover:border-[2px] hover:m-[2px] hover:drop-shadow-md`}></div>;
+"use client";
+const ChangeCTA = ({ bg_color, active, setcurrentWatchColor, currentWatchColor }) => {
+  // if (currentWatchColor === currentWatchColor) {
+  //   active = "h-[26px] w-[26px] border-[2px] m-[2.5px] drop-shadow-md";
+  // }
+
+  return (
+    <div
+      className={`h-[24px] w-[24px] rounded-full border-[1px] border-white ${bg_color} m-[3px] cursor-pointer hover:h-[26px] hover:w-[26px] hover:border-[2px] hover:m-[2.5px] hover:drop-shadow-md  ${active}`}
+      onClick={() => {
+        setcurrentWatchColor(currentWatchColor);
+      }}></div>
+  );
 };
 
 export default ChangeCTA;
